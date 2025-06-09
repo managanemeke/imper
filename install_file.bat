87f6435b370a689a2c5f161aadb1224839e0ac7d
managanemeke <managanemeke@gmail.com>
2025-06-09 12:15:22 +0500

    feat(install_file): initialize

---
+++ install_file.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+CHCP 65001
+
+SET "path=%~dp0"
+SET "name=Ночная статика"
+SET "position=Bottom"
+SET command=%path%file.bat ""%%1""
+
+CALL :add_context_menu_item_for_extension ".png"
+CALL :add_context_menu_item_for_extension ".jpg"
+CALL :add_context_menu_item_for_extension ".jpeg"
+
+EXIT /B %ERRORLEVEL%
+
+:add_context_menu_item_for_extension
+SET "type=%~1"
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f /v MUIVerb /t REG_SZ /d %name%
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f /v Position /t REG_SZ /d %position%
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%\command" /f
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%\command" /f /ve /d "%command%"
+EXIT /B 0
