b1facd618e6ef249a5f8e7d9fb296494de44a6b6
managanemeke <managanemeke@gmail.com>
2025-06-30 21:35:56 +0500

    feat(install_file): set nightify name to "1_1_nightify"

--- install_file.ps1
+++ install_file.ps1
@@ -68,4 +68,4 @@ function Add-ContextMenuCommand {
 Add-ContextMenuCommand "HKCR\*" "imper" "Незам" "Bottom" "true" ""
 
 $command = "`"$((Get-Command powershell).Source)`" -NoProfile -ExecutionPolicy Bypass -File `"$scriptPath\file.ps1`" `"`"`%1`"`""
-Add-ContextMenuCommand "HKCR\*\shell\imper" "Ночная статика" "Ночная статика" "Bottom" "false" $command
+Add-ContextMenuCommand "HKCR\*\shell\imper" "1_1_nightify" "Ночная статика" "" "false" $command
