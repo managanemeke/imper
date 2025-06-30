8b5d3a352a2616eb5db88482f88f8020fe6f61f8
managanemeke <managanemeke@gmail.com>
2025-06-30 22:00:07 +0500

    refactor(install_menu): rename from install_file.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 ﻿Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PWD\install_dependencies.ps1`""
 
-Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PWD\install_file.ps1`"" -Verb RunAs
+Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PWD\install_menu.ps1`"" -Verb RunAs
 
 Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PWD\check_dependencies.ps1`""
