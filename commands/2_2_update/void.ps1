0404f6bc89e4019e38ed62209bf6b7fa0d446679
managanemeke <managanemeke@gmail.com>
2025-07-01 19:14:27 +0500

    feat(2_2_update): use one file https git cloning technique

--- commands/2_2_update/void.ps1
+++ commands/2_2_update/void.ps1
@@ -4,4 +4,17 @@ Set-Location -Path $scriptDirectory
 Set-Location ..
 Set-Location ..
 
-git pull --recurse-submodules origin main
+$repository = "https://github.com/managanemeke/meeseeks-box-structures.git"
+$branch = "main"
+$fileName = "structures.csv"
+$filePath = "$fileName"
+$directory = "structures"
+
+Remove-Item -Path $directory -Recurse -Force
+
+git clone --depth 1 --branch $branch --filter=blob:none --no-checkout $repository $directory
+
+Set-Location $directory
+git sparse-checkout init --no-cone
+git sparse-checkout set --no-cone "$filePath"
+git checkout $branch
